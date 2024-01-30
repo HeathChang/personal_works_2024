8f41b5fad36ad1b44f0fcdf2ea997b3224147d2f
heathchang <jannhyunsoo@gmail.com>
2024-01-30 21:54:26 +0900

    add test

--- coding_test_2024/leet_top_interview_150/008M.ZigzagConversion(6).js
+++ coding_test_2024/leet_top_interview_150/008M.ZigzagConversion(6).js
@@ -2,7 +2,26 @@
 // RUN-TIME >
 // Memory >
 const solution = (s, numRows) => {
-    if (numRows === 1) return s;
+    if (numRows === 1 || s.length <= numRows) {
+        return s;
+    }
+
+    const result = Array.from({length: numRows}, () => []);
+
+    let direction = 1; // 1 represents going down, -1 represents going up
+    let row = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        result[row].push(s[i]);
+        row += direction;
+
+        if (row === 0 || row === numRows - 1) {
+            direction *= -1;
+        }
+
+    }
+
+    return result.flat().join("");
 
 };
 
---
+++ coding_test_2024/leet_top_interview_150/009M.SpiralMatrix(54).js
@@ -0,0 +1,24 @@
+// URL > https://leetcode.com/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+
+const solution = (matrix) => {
+    let width = matrix[0].length;
+    let height = matrix.length;
+    let result = [];
+    const newMatrix = matrix.flat();
+    console.log(2, newMatrix);
+    for (let i = 0; i < newMatrix.length; i++) {
+
+    }
+
+    return result;
+};
+
+const result = solution([ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ] ]);
+console.log("result:: ", result);
+///
+
+/**
+ * ```
+
+ * ```
+ * */
