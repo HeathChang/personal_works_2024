d370bd3cbafe7a05657e6e294b8aebc50df60245
heathchang <jannhyunsoo@gmail.com>
2024-03-01 21:53:27 +0900

    daily-test

---
+++ coding_test_2024/programmers_code_challenge/lv-?/002.sort.js
@@ -0,0 +1,52 @@
+function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+let arr = [38, 27, 43, 3, 9, 82, 10];
+console.log(quickSort(arr)); // [3, 9, 10, 27, 38, 43, 82]
+
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+
+console.log(mergeSort(arr)); // [3, 9, 10, 27, 38, 43, 82]
