5a8cd8fb69440e4b60f6f02acae9e6f0c950d93d
heathchang <jannhyunsoo@gmail.com>
2024-03-06 23:03:10 +0900

    1.coding test-fail

---
+++ coding_test_2024/programmers_code_challenge/lv-1/133499.babbling.js
@@ -0,0 +1,9 @@
+// URL >
+
+const solution = (babbling) => {
+    const ableBabblingArray = ["aya", "ye", "woo", "ma"]
+    // 실패
+};
+
+console.log("result:: ", solution(["aya", "yee", "u", "maa"])); // 1
+console.log("result:: ", solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])); // 2
