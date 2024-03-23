659e720553d466a67c4a5dade7c39e51ac524e03
heathchang <jannhyunsoo@gmail.com>
2024-03-23 17:28:01 +0900

    coding practice

--- coding_test_2024/programmers_code_challenge/lv-1/64061.kakao-claw-machine.js
+++ coding_test_2024/programmers_code_challenge/lv-1/64061.kakao-claw-machine.js
@@ -3,7 +3,9 @@
 const solution = (board, moves) => {
     const N = board.length;
     let newArr = [];
-    let result = []
+    let result = [];
+    let count = 0
+    let flag = true;
     for(let i = 0; i < N ; i++ ){
         newArr[i] = new Array(N).fill(-1)
     }
@@ -12,19 +14,41 @@ const solution = (board, moves) => {
         for(let j = 0; j < board.length; j++){
             newArr[j][i]=board[i][j]
         }
-    }
-    for(let i = 0 ; i < moves.length;i++){
+    }    for(let i = 0 ; i < moves.length;i++){
         for(let j = 0 ; j < moves.length; j++){
             if(newArr[moves[i]-1][j] === 0) {
                 continue;
             }
-            result.push(newArr[moves[i]-1][j])
+            const value = newArr[moves[i]-1][j]
+            if(value){
+                result.push(value)
+            }
             newArr[moves[i]-1][j] = 0
             break;
         }
     }
+    const deleteDuplicate = (arr) => {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] === arr[i + 1]) {
+                arr.splice(i, 2);
+                i--;
+                count += 1
+            }
+        }
+        return arr
+    }
+    const checkDuplicate = (arr) => {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] === arr[i + 1]) return true
+        }
+        return false
+    }
 
-    console.log(result)
+    while(checkDuplicate(result)){
+        result = deleteDuplicate(result)
+    }
+
+    return count * 2
 };
 
 console.log("result:: ", solution([ [ 0, 0, 0, 0, 0 ], [ 0, 0, 1, 0, 3 ], [ 0, 2, 5, 0, 1 ], [ 4, 2, 4, 4, 2 ], [ 3, 5, 1, 3, 1 ] ], [ 1, 5, 3, 5, 1, 2, 1, 4 ]));
