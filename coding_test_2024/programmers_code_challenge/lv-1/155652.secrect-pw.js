51f1a1da4e9e4d7359cbd5425abd9d746ae07b54
heathchang <jannhyunsoo@gmail.com>
2024-03-24 21:42:51 +0900

    coding practice

---
+++ coding_test_2024/programmers_code_challenge/lv-1/155652.secrect-pw.js
@@ -0,0 +1,12 @@
+// URL > https://school.programmers.co.kr/learn/courses/30/lessons/155652
+
+const solution = (s, skip, index) => {
+    let alphabetArr = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
+    let sArr = s.split("");
+    let skipArr = skip.split("");
+    
+    for (let i = 0; i < sArr.length; i++) {
+    }
+};
+
+console.log("result:: ", solution("aukks", "wbqd", 5));
