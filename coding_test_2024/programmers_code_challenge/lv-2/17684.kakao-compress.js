53e31399526e09b5a50ff5ea986c4cf4830ce5e7
heathchang <jannhyunsoo@gmail.com>
2024-04-01 23:48:30 +0900

    coding practice

--- coding_test_2024/programmers_code_challenge/lv-2/17684.kakao-compress.js
+++ coding_test_2024/programmers_code_challenge/lv-2/17684.kakao-compress.js
@@ -1,5 +1,20 @@
 // URL > https://school.programmers.co.kr/learn/courses/30/lessons/17684
 
+// const solution = (msg) => {
+//     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     let tempArr = [];
+//     let result = [];
+//     let msgSplit = msg.split("")
+//     for (let i = 0; i < alphabet.length; i++) {
+//         tempArr.push(alphabet[i]);
+//     }
+//
+//
+//
+//     return result;
+// };
+
+
 const solution = (msg) => {
     const dict = ['*', ...Array.from(Array(26), (_, i) => String.fromCharCode(65 + i))];
     const arr = [];
@@ -8,22 +23,25 @@ const solution = (msg) => {
     let wordIdxInDict = 0;
     for (let idx = 0; idx < msg.length; idx++) {
         word += msg[idx];
+        console.log("current word::: ",idx, word)
         const tempWordIdxInDict = dict.indexOf(word);
         if (tempWordIdxInDict !== -1) {
             // 있는 경우
             wordIdxInDict = tempWordIdxInDict;
-            // console.log("not clear",word, wordIdxInDict)
+            console.log("next idx",word, wordIdxInDict)
         } else {
             // 없는 경우
             dict.push(word);
             arr.push(wordIdxInDict);
+            console.log("reset word:: ", word)
             word = '';
             idx--; // 다시 시작
+            console.log("restart current idx:: ", idx)
         }
     }
     return [...arr, wordIdxInDict];
 }
 
 console.log("result:: ", solution("KAKAO"));
-console.log("result:: ", solution("TOBEORNOTTOBEORTOBEORNOT")); //  [20, 15,  2,  5, 15, 18,14, 15, 20, 27, 29, 31,36, 30, 32, 34]
-console.log("result:: ", solution("ABABABABABABABAB")); // [1, 2, 27, 29, 28, 31, 30]
+// console.log("result:: ", solution("TOBEORNOTTOBEORTOBEORNOT")); //  [20, 15,  2,  5, 15, 18,14, 15, 20, 27, 29, 31,36, 30, 32, 34]
+// console.log("result:: ", solution("ABABABABABABABAB")); // [1, 2, 27, 29, 28, 31, 30]
