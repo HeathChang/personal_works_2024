6b14440ac45c857214bb2b414a8642dce4b3ecdd
heathchang <jannhyunsoo@gmail.com>
2024-04-05 22:24:18 +0900

    night coding

--- coding_test_2024/programmers_code_challenge/lv-1/140108.split-string.js
+++ coding_test_2024/programmers_code_challenge/lv-1/140108.split-string.js
@@ -1,9 +1,34 @@
 // URL > https://school.programmers.co.kr/learn/courses/30/lessons/140108
 // 문제 이해 못함.
 const solution = (s) => {
-    return n.split("")
+    let count = 0;
+    let first = s[0];
+    let skipCount = 0;
+    for (let i = 1; i <= s.length; ++i) {
+        if (s[i] === undefined) {
+            count += 1;
+            break;
+        }
+
+        if (first === s[i]) {
+            skipCount += 1;
+        } else {
+            if (skipCount === 0) {
+                count += 1;
+                first = s[i + 1];
+                skipCount = 0;
+                i++;
+            } else {
+                skipCount -= 1;
+            }
+        }
+    }
+
+    return count;
 };
 
-console.log("result:: ", solution("banana")); // 3
-console.log("result:: ", solution("abracadabra")); // 6
-console.log("result:: ", solution("aaabbaccccabba")); //3
+console.log("result:: ", solution("baaa")); // 2
+// console.log("result:: ", solution("banana")); // 3
+// console.log("result:: ", solution("abracadabra")); // 6
+// console.log("result:: ", solution("aaabbaccccabba")); //3
+//
