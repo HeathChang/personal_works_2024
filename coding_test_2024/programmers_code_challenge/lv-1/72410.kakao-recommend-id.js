2a391a107eefe2c453a7e5a7f869d6b3e9c6e808
heathchang <jannhyunsoo@gmail.com>
2024-04-06 23:14:46 +0900

    night coding - 72410.unfin

---
+++ coding_test_2024/programmers_code_challenge/lv-1/72410.kakao-recommend-id.js
@@ -0,0 +1,15 @@
+// URL > https://school.programmers.co.kr/learn/courses/30/lessons/72410
+
+const solution = (new_id) => {
+    let result = "";
+
+
+    return result;
+};
+
+
+console.log("result:: ", solution("...!@BaT#*..y.abcdefghijklm")); //"bat.y.abcdefghi"
+console.log("result:: ", solution("z-+.^.")); //"z--"
+console.log("result:: ", solution("=.=")); // "aaa"
+console.log("result:: ", solution("123_.def")); //"123_.def"
+console.log("result:: ", solution("abcdefghijklmn.p")); //"abcdefghijklmn"
