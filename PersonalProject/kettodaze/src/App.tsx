d37ef6e1e633cc5fe7b5533db782e30b616300dd
heath <jannhyunsoo@gmail.com>
2024-05-16 23:13:59 +0900

    # kettodaze-initial-setup-ts

---
+++ PersonalProject/kettodaze/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestIndex from "@/pages/Test/TestIndex";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <TestIndex/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ PersonalProject/kettodaze/src/pages/Test/TestIndex.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestIndex: React.FC = () => {
+    return <div>This is a test div</div>;
+};
+
+export default TestIndex;
