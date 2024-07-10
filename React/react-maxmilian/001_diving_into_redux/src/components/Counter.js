4b738d135d0b34966b4bfde5ca2292af915666d1
heath <jannhyunsoo@gmail.com>
2024-07-11 00:33:13 +0900

    redux-review-1 - redux setup - useSelector & useDispatch

--- React/react-maxmilian/001_diving_into_redux/src/components/Counter.js
+++ React/react-maxmilian/001_diving_into_redux/src/components/Counter.js
@@ -1,15 +1,27 @@
 import classes from './Counter.module.css';
+import {useSelector, useDispatch} from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const {counter} = useSelector(state => state); // redux store 의 state 조회하며, 변화를 감지하는 hook
+    const dispatch = useDispatch();
+    const toggleCounterHandler = () => {
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    };
+
+    const increment = () => {dispatch({type: 'increment'})}
+    const decrement = () =>  {dispatch({type: 'decrement'})}
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={increment}>increment</button>
+                <button onClick={decrement}>decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
 };
 
 export default Counter;
