60eafd8572a1a9a6009ab4831c7205148148d96e
heath <jannhyunsoo@gmail.com>
2024-07-12 00:41:33 +0900

    redux-review-2 - useSelector 1:1 - reducer overwrite

--- React/react-maxmilian/001_diving_into_redux/src/components/Counter.js
+++ React/react-maxmilian/001_diving_into_redux/src/components/Counter.js
@@ -2,21 +2,26 @@ import classes from './Counter.module.css';
 import {useSelector, useDispatch} from "react-redux";
 
 const Counter = () => {
-    const {counter} = useSelector(state => state); // redux store 의 state 조회하며, 변화를 감지하는 hook
+    const counter = useSelector(state => state.counter); // redux store 의 state 조회하며, 변화를 감지하는 hook
+    const show = useSelector(state => state.showCounter); // redux store 의 state 조회하며, 변화를 감지하는 hook
+    // useSelector.js:76 Selector unknown returned the root state when called. This can lead to unnecessary rerenders.
+    // useSelector 은 1:1 로 매칭해야함 > 안할 시, 모든 변경 사항으로 인해 불필요한 재렌더링 유발 가능
+
     const dispatch = useDispatch();
-    const toggleCounterHandler = () => {
 
-    };
-
-    const increment = () => {dispatch({type: 'increment'})}
+    const increment = () => {dispatch({type: 'increment',})}
     const decrement = () =>  {dispatch({type: 'decrement'})}
+    const increase = () =>  {dispatch({type: 'increase', amount: 5})}
+
+    const toggleCounterHandler = () => {dispatch({type: 'toggle'})}
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter}</div>
+            {show && <div className={classes.value}>{counter}</div>}
             <div>
                 <button onClick={increment}>increment</button>
+                <button onClick={increase}>increase</button>
                 <button onClick={decrement}>decrement</button>
             </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- React/react-maxmilian/001_diving_into_redux/src/store/index.js
+++ React/react-maxmilian/001_diving_into_redux/src/store/index.js
@@ -1,8 +1,23 @@
 import {createStore} from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type === 'increment') {return {counter: state.counter+1}}
-    else if(action.type === 'decrement') {return {counter: state.counter - 1}}
+const initState = {counter: 0, showCounter: true};
+const counterReducer = (state = initState, action) => {
+    if (action.type === 'increment') {
+        return { ...state , counter: state.counter + 1};
+    } else if (action.type === 'decrement') {
+        // return state < X
+        // return { counter: state.counter + 1, counter: state.counter - 1}; < O
+        // never mutate original state, but return brand-new state
+
+        // IMPORTANT::  these returned object will not be merged with the existing state but overwrite the existing state.
+        return { ...state, counter: state.counter - 1};
+    } else if (action.type === 'increase') {
+        return { ...state, counter: state.counter + action.amount};
+    } else if (action.type === 'toggle'){
+        console.log('action', state, action)
+        return { ...state, showCounter: !state.showCounter}
+    }
+
     return state;
 };
 
