6a88df32a7f9f9136729d30dd220dea57bb39bdd
heath <jannhyunsoo@gmail.com>
2024-07-17 14:27:58 +0900

    redux-saga-starwars -setup

---
+++ React/react-redux-saga/002_redux-saga-starwars/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <header>Star Wars API</header>
+
+        </div>
+    );
+};
+
+export default App;
