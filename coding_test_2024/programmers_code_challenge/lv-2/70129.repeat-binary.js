fb51a484cade6252527b4318a9bbc6acfee6f789
heath <jannhyunsoo@gmail.com>
2024-07-22 23:24:04 +0900

    ct: programmers-70129-solved

--- coding_test_2024/programmers_code_challenge/lv-2/70129.repeat-binary.js
+++ coding_test_2024/programmers_code_challenge/lv-2/70129.repeat-binary.js
@@ -1,27 +1,20 @@
 // URL > https://school.programmers.co.kr/learn/courses/30/lessons/70129
 
 const solution = (s) => {
-    let val = `${s}`;
-    let count = 0;
-    let outerCount = 0;
-    while(val !== '1'){
-        `${val}`.split('').forEach(item => {
-            if(+item === 0){
-                console.log(1)
-                count+=1;
-            } else {
-                val += '1';
-            }
+    let countConversions = 0;  // 이진 변환 횟수
+    let countZerosRemoved = 0; // 제거된 0의 개수
 
-
-            outerCount+=1;
-        })
-
-        val = '';
+    while (s !== "1") {
+        let zeros = (s.match(/0/g) || []).length; // 현재 문자열에서 0의 개수
+        countZerosRemoved += zeros; // 제거된 0의 총 개수에 추가
+        s = s.replace(/0/g, ''); // 문자열에서 0을 모두 제거
+        s = s.length.toString(2); // 문자열의 길이를 이진수로 변환
+        countConversions += 1; // 이진 변환 횟수 증가
     }
-    return [outerCount, count]
+
+    return [countConversions, countZerosRemoved];
 };
 
 console.log("result:: ", solution('110010101001')); // [3,8]
-// console.log("result:: ", solution(01110)); //[3,3]
-// console.log("result:: ", solution(1111111)); //[4,1]
+console.log("result:: ", solution('01110')); //[3,3]
+console.log("result:: ", solution('1111111')); //[4,1]
