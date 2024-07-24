87ce8bf3e1851e276ae47bd95aa99a100cf89bb8
heath <jannhyunsoo@gmail.com>
2024-07-24 23:59:45 +0900

    ct: programmers-12914-solved-refactor

--- coding_test_2024/programmers_code_challenge/lv-2/12914.longJump.js
+++ coding_test_2024/programmers_code_challenge/lv-2/12914.longJump.js
@@ -1,20 +1,16 @@
-// URL >
+// URL > https://school.programmers.co.kr/learn/courses/30/lessons/12914
 
 const solution = (n) => {
-    const MOD = 1234567;
+	if (n <= 2) return n;
+	const dp = new Array(n + 1).fill(0);
+	dp[1] = 1;
+	dp[2] = 2;
 
-    // 기본적인 경우
-    if (n === 1) return 1;
-    if (n === 2) return 2;
+	for (let i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+	}
+    return dp[n]
 
-    let dp = new Array(n + 1).fill(0);
-    dp[1] = 1;
-    dp[2] = 2;
-
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-    }
-    return dp[n];
 };
 
 console.log("result:: ", solution(4)); // 5
