fc62cce3bb16075a9d471c8dc2d329be7406bfca
heath <jannhyunsoo@gmail.com>
2024-07-25 00:10:01 +0900

    ct:programmers-12985-solved

---
+++ coding_test_2024/programmers_code_challenge/lv-2/12985.expect-bracket.js
@@ -0,0 +1,15 @@
+// URL > https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+const solution = (n, a, b) => {
+	if (n === 2) return 1;
+	let count = 0
+	while (a !== b) {
+		a = Math.ceil(a / 2);
+		b = Math.ceil(b / 2);
+		count++;
+	}
+	return count;
+};
+
+console.log("result:: ", solution(8, 4, 7)); //3
+console.log(Math.log2(8))
