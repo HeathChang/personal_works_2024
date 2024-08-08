728166190fd7d2e7ed1afca2c48e803192928501
heath <jannhyunsoo@gmail.com>
2024-08-08 23:30:37 +0900

    ct: programmers-42586-solved-refactor

--- coding_test_2024/programmers_code_challenge/lv-2/42586.development.js
+++ coding_test_2024/programmers_code_challenge/lv-2/42586.development.js
@@ -1,34 +1,25 @@
-// URL >
+// URL > https://school.programmers.co.kr/learn/courses/30/lessons/42586
 
 const solution = (progresses, speeds) => {
-	let tempArr = [];
 	let resultArr = [];
+	const tempArr = progresses.map((item, i) => Math.ceil(( 100 - progresses[i] ) / speeds[i]));
 
-	for (let i = 0; i < progresses.length; i++) {
-		let remainProgress = Math.ceil(( 100 - progresses[i] ) / speeds[i]);
-		tempArr.push(remainProgress);
-	}
 	let point = -1;
 	let count = 0;
 	for (let i = 0; i < tempArr.length; i++) {
-
-        if(point === -1) {
-            count += 1;
-            point = tempArr[i]
-        } else  if (point >= tempArr[i]) {
-            count += 1;
-        } else {
-            resultArr.push(count);
-            count = 1;
-            point = tempArr[i]
-        }
-        if(i === tempArr.length-1) resultArr.push(count)
+		if (point === -1 || point >= tempArr[i]) {
+			count += 1;
+		} else {
+			resultArr.push(count);
+			count = 1;
+		}
+		point = tempArr[i];
+		if (i === tempArr.length - 1) resultArr.push(count);
 
 	}
 	return resultArr;
 };
 
 console.log("result:: ", solution([93, 30, 55], [1, 30, 5]));
-console.log("@@@@@@@@")
 console.log("result:: ", solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
 
