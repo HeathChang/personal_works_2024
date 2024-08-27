ff4c2d1ea490be6705a52acd9797626feb06fd0e
heath <jannhyunsoo@gmail.com>
2024-08-27 23:57:55 +0900

    react-testing-vitest-101 -test button color context change

--- React/react-testing-vitest/000_basic101/src/App.test.jsx
+++ React/react-testing-vitest/000_basic101/src/App.test.jsx
@@ -1,9 +1,36 @@
-import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { logRoles } from "@testing-library/react";
 
-test("App contains correct heading", () => {
-	render(<App/>);
-	// const headingElement = screen.getByText(/learn react/i); // find element by display text
-	const roleElement = screen.getByRole('heading', { name: /learn react/i });
-	expect(roleElement).toBeInTheDocument(); // assertion
+const startPoint = () => {
+	const { container } = render(<App/>);
+	// logRoles(container); // <button class= "red"/ > 노출
+
+};
+
+const checkButton = () => {
+	return screen.getByRole("button", { name: /blue/i });
+};
+
+test("button before click with correct color and label", () => {
+	startPoint();
+	const buttonElement = checkButton();
+
+	// check button context before click
+	expect(buttonElement).toHaveTextContent(/blue/i);
+
+	// check button color before click
+	expect(buttonElement).toHaveClass("red"); //  특정 DOM 요소에 특정한 CSS 클래스
+
+});
+
+test("button after click with correct color and label", () => {
+	startPoint();
+	const buttonElement = checkButton();
+	fireEvent.click(buttonElement);// click button
+
+	// check button context after click
+	expect(buttonElement).toHaveTextContent(/red/i);
+	// check button color after click
+	expect(buttonElement).toHaveClass("blue");
 });
