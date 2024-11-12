63505a18d3a90227014cb02accebc2827c9bf9f7
heath <jannhyunsoo@gmail.com>
2024-11-12 23:43:10 +0900

    ct: leetcode-228

--- coding_test_2024/leet_top_interview_150/easy/228.summary-ranges.js
+++ coding_test_2024/leet_top_interview_150/easy/228.summary-ranges.js
@@ -1,9 +1,25 @@
 // URL > https://leetcode.com/problems/summary-ranges/?envType=study-plan-v2&envId=top-interview-150
 
 const solution = (nums) => {
-    const a = new Set(nums);
-    nums.length = 0;
-    console.log(nums)
+    let result = [];
+    let start = nums[0]; // Start of the current range
+
+    for (let i = 1; i <= nums.length; i++) {
+        // If current number is not consecutive, we end the current range
+        if (i === nums.length || nums[i] !== nums[i - 1] + 1) {
+            if (start === nums[i - 1]) {
+                // Single element range
+                result.push(`${start}`);
+            } else {
+                // Range with more than one element
+                result.push(`${start}->${nums[i - 1]}`);
+            }
+            // Update start to the new range
+            start = nums[i];
+        }
+    }
+
+    return result;
 };
 
 console.log("result:: ", solution([0,1,2,4,5,7]));   // ["0->2","4->5","7"]
