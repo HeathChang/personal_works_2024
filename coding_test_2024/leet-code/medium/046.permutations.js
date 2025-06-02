eff98cce6a85cc6f643f3f765e2ce27e9714d130
heath <jannhyunsoo@gmail.com>
2025-06-02 14:53:18 +0900

    ct: leet-code -046

---
+++ coding_test_2024/leet-code/medium/046.permutations.js
@@ -0,0 +1,28 @@
+// URL > https://leetcode.com/problems/permutations/
+
+const solution = (nums) => {
+	nums.sort();
+	const returnArray = [];
+	const usedArray = Array(nums.length).fill(false);
+
+	const backTrack = (path) => {
+		if (path.length === nums.length) {
+			returnArray.push([...path]);
+			return;
+		}
+
+		for (let i = 0; i < nums.length; i++) {
+			if (usedArray[i]) continue;
+			usedArray[i] = true;
+			path.push(nums[i]);
+			backTrack(path);
+			path.pop();
+			usedArray[i] = false;
+		}
+	};
+
+	backTrack([]);
+	return returnArray;
+};
+
+console.log("result:: ", solution([1, 2, 3])); //  [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
