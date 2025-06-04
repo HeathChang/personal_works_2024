97517992eb15ab1526d54d8a25eff5e02185a143
heath <jannhyunsoo@gmail.com>
2025-06-04 15:48:08 +0900

    ct: leet-code -078

---
+++ coding_test_2024/leet-code/medium/078.subsets.js
@@ -0,0 +1,26 @@
+// URL > https://leetcode.com/problems/sort-colors/
+
+const solution = (nums) => {
+	if (nums.length === 1) return [[], nums];
+	const returnArray = [];
+
+	const backTrack = (index, path) => {
+        returnArray.push([...path]);
+
+        for (let i = index; i < nums.length; i++) {
+			path.push(nums[i]);
+			backTrack(i + 1, path);
+            console.log('before::',path)
+			path.pop();
+            console.log('after::',path)
+		}
+	};
+    backTrack(0, []);
+
+    return returnArray;
+
+
+};
+
+console.log("result:: ", solution([1, 2, 3])); // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// console.log("result:: ", solution([0])); // [[],[0]]
