f03825478567a58b558d17f2f70e62055ee26f01
heath <jannhyunsoo@gmail.com>
2025-06-11 16:05:05 +0900

    ct: leet-code -053

---
+++ coding_test_2024/leet-code/medium/053.maximum-subarray.js
@@ -0,0 +1,16 @@
+// URL > https://leetcode.com/problems/maximum-subarray/description/
+
+const solution = (nums) => {
+	if (nums.length === 1) return nums[0];
+	let maxSum = nums[0];
+	let currentSum = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		currentSum = Math.max(nums[i], maxSum + nums[i]);
+		maxSum = Math.max(maxSum, currentSum);
+	}
+	return maxSum;
+};
+
+console.log("result:: ", solution([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 2
+console.log("result:: ", solution([1])); // 1
