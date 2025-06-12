37a47e7967a7c66fb8f8a3a541b3c4661cb1489c
heath <jannhyunsoo@gmail.com>
2025-06-12 18:40:17 +0900

    ct: leet-code -322 fix

--- coding_test_2024/leet-code/medium/322.coin-change.js
+++ coding_test_2024/leet-code/medium/322.coin-change.js
@@ -1,21 +1,18 @@
 // URL > https://leetcode.com/problems/coin-change/description/?envType=study-plan-v2&envId=top-interview-150
 
 const solution = (coins, amount) => {
-    // 1st att
-	// if (amount === 0) return 0;
-	// if (coins.length === 1 && coins[0] > amount) return -1;
-    //
-	// const dp = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
-	// dp[0] = 0;
-	// for (let coin of coins) {
-	// 	for (let i = coin; i <= amount; i++) {
-	// 		dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-	// 	}
-	// }
-	// return dp[amount] === Number.MAX_SAFE_INTEGER ? -1 : dp[amount];
+    // FIRST ATTEMPT
+	if (amount === 0) return 0;
+	if (coins.length === 1 && coins[0] > amount) return -1;
 
-
-
+	const dp = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
+	dp[0] = 0;
+	for (let coin of coins) {
+		for (let i = coin; i <= amount; i++) {
+			dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+		}
+	}
+	return dp[amount] === Number.MAX_SAFE_INTEGER ? -1 : dp[amount];
 };
 
 console.log("result:: ", solution([1, 2, 5], 11)); // 3
