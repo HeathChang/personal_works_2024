47a095405f06f630937ecc2d594db25a7e41fe9d
heath <jannhyunsoo@gmail.com>
2025-07-18 16:18:55 +0900

    algorithm -quick-select-abm

--- coding_test_2024/algorithm-based/sort/quickSelect.js
+++ coding_test_2024/algorithm-based/sort/quickSelect.js
@@ -1,36 +1,27 @@
 // quick-select
 
 const solution = (nums, k) => {
-	const quickSelect = (arr, left, right, kIndex) => {
+	const quickSelect = (arr, left, right, targetK) => {
 		if (left === right) return arr[left];
-		console.log('quickSelect:: ',  arr, left, right, kIndex)
-
-		const pivotIndex = partition(arr, left, right);
-		console.log(arr, pivotIndex, kIndex)
-		if (pivotIndex === kIndex) {
-			return arr[pivotIndex]; // 정답!
-		} else if (pivotIndex > kIndex) {
-			// 왼쪽에서 다시 찾기
-			return quickSelect(arr, left, pivotIndex - 1, kIndex);
-		} else {
+		const pivotIndex = sort(arr, left, right);
+		if (targetK === pivotIndex) return arr[pivotIndex]; else if (pivotIndex > targetK) {
+			// 왼쪽 다시찾기
+			return quickSelect(arr, 0, pivotIndex - 1, targetK);
+		} else if (pivotIndex < targetK) {
 			// 오른쪽에서 다시 찾기
-			return quickSelect(arr, pivotIndex + 1, right, kIndex);
+			return quickSelect(arr, 0, pivotIndex + 1, targetK);
 		}
-
 	};
 
-	const partition = (arr, left, right) => {
-		const pivot = arr[right]; // 마지막 값을 피벗으로 선택
-		console.log('pivot:: ', pivot, arr, left, right)
-		let i = left; // 작은 값들이 모이는 구역
-
+	const sort = (arr, left, right) => {
+		let pivotValue = arr[right]; // 4 -> 6 -> 5
+		let i = left;
 		for (let j = left; j < right; j++) {
-			if (arr[j] > pivot) {
-				[arr[i], arr[j]] = [arr[j], arr[i]]; // swap
+			if (arr[j] > pivotValue) {
+				[arr[i], arr[j]] = [arr[j], arr[i]];
 				i++;
 			}
 		}
-
 		[arr[i], arr[right]] = [arr[right], arr[i]];
 		return i;
 	};
