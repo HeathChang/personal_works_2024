fd1276342a53bed94f4fc7d9d39171b8e6b54343
null_ong2 <jannhyunsoo@gmail.com>
2025-09-12 21:32:49 +0900

    leet-code:122

---
+++ coding_test_2024/leet-code/medium/122.best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,15 @@
+// URL > https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description
+
+const solution = (prices) => {
+    let maxProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1];
+        }
+    }
+    
+    return maxProfit;
+};
+
+console.log("result:: ", solution([7,1,5,3,6,4]));
