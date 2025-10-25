cefc68d3c86237a5addcf32251000dd415d3ecae
null_ong2 <jannhyunsoo@gmail.com>
2025-10-25 20:15:04 +0900

    [react-query] react-query-101

--- React/react-query/base-blog-em/src/Posts.jsx
+++ React/react-query/base-blog-em/src/Posts.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
+
 const maxPostPage = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['posts', currentPage, 'check'], // queryKey: 응답데이터의 unique key로 데이터를 캐싱할 때 사용되며, 해당 queryKey가 변경될때 쿼리가 업데이트
+    queryFn: () => fetchPosts(currentPage), // queryFn: Promise를 반환하는 함수로 API 요청이 들어간다.
+    staleTime: 2000, // 2초 동안 데이터를 캐싱
+  })
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data?.map((post) => (
           <li
             key={post.id}
             className="post-title"
@@ -25,11 +34,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled onClick={() => { }}>
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button disabled onClick={() => { }}>
           Next page
         </button>
       </div>
