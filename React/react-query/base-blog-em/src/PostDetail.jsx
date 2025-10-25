572f5894b23804e7a67704ddc66a5ead2b0c4408
null_ong2 <jannhyunsoo@gmail.com>
2025-10-26 07:49:57 +0900

    [react-query] react-query-101 - 2

--- React/react-query/base-blog-em/src/PostDetail.jsx
+++ React/react-query/base-blog-em/src/PostDetail.jsx
@@ -1,13 +1,21 @@
 import { fetchComments } from "./api";
 import "./PostDetail.css";
+import { useQuery } from "@tanstack/react-query";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+    staleTime: 1000 * 60 * 5,
+  })
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: "blue" }}>POST DETAIL:: {post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
